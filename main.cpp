1a9669bff1bc961dc857acf44291415519a2f8d0
Dinan Yin <steroidg@gmail.com>
2013-05-11 00:10:53 +1000

    Handled the first event

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
-
-
 #include <SDL_image.h>
 #include <SDL.h>
 #include <iostream>
 
+	const int SCREEN_WIDTH	= 640;
+	const int SCREEN_HEIGHT = 480;
+	const int SCREEN_BPP = 32;
 
 // Dave put this here as part of the example
 // I guess this uses the std namespace so I don't need to do stuffs like std::string
 using namespace std;
 
+
+/*
+ * This function applies one surface onto another.
+ */
 void apply_surface (int x, int y, SDL_Surface *src, SDL_Surface *dest) {
 	SDL_Rect offset;
 	offset.x = x;
@@ -17,7 +22,10 @@ void apply_surface (int x, int y, SDL_Surface *src, SDL_Surface *dest) {
 	SDL_BlitSurface( src, NULL, dest, &offset );
 }
 
-SDL_Surface *load_image( string filename ) {
+/*
+ * This function loads a image and return the correct bpp
+ */
+SDL_Surface *load_image (string filename) {
 	SDL_Surface *loaded_image = NULL;
 	SDL_Surface *optimized_image = NULL;
 	
@@ -29,33 +37,52 @@ SDL_Surface *load_image( string filename ) {
 	return optimized_image;
 }
 
+
+bool init (SDL_Surface **screen) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1) {
+		printf ("%s %d ERROR: Unable to SDL_Init.\n", __PRETTY_FUNCTION__, __LINE__);
+		return false;
+	}
+	
+	*screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_HWSURFACE );
+	if (! *screen) {
+		printf ("%s %d ERROR: Unable to create screen.\n", __PRETTY_FUNCTION__, __LINE__);
+		return false;
+	}
+	SDL_WM_SetCaption( "Dao", NULL );
+	return true;
+}
+
+
+
 int main( int argc, char *argv[] ) {
-    SDL_Init( SDL_INIT_EVERYTHING );
-	
-	const int SCREEN_WIDTH	= 640;
-	const int SCREEN_HEIGHT = 480;
-	const int SCREEN_BPP = 32;
-	
+	SDL_Event event;
     SDL_Surface *screen = NULL;
 	SDL_Surface *dao = NULL;
 	SDL_Surface *bg = NULL;
 	
-	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_HWSURFACE );
+	//Make sure the program waits for a quit 
+	bool quit = false;
+	
+	if (init (&screen) == false) {
+			printf ("%s %d ERROR: Unable to init.\n", __PRETTY_FUNCTION__, __LINE__);
+			return 1;
+	}
+	
 	if (! screen) {
-		printf ("ERROR: Unable to create screen.");
+		printf ("%s %d ERROR: Unable to create screen.\n", __PRETTY_FUNCTION__, __LINE__);
 		return 1;
 	}
-	SDL_WM_SetCaption( "Dao", NULL );
 	
 	dao = load_image("images/dao.png");
 	if (! dao) {
-		printf ("ERROR: Unable to load bitmap.");
+		printf ("%s %d ERROR: Unable to load bitmap.\n", __PRETTY_FUNCTION__, __LINE__);
 		return 1;
 	}
 	
 	bg = load_image("images/background.png");
 	if (! bg) {
-		printf ("ERROR: Unable to load bitmap.");
+		printf ("%s %d ERROR: Unable to load bitmap.\n", __PRETTY_FUNCTION__, __LINE__);
 		return 1;
 	}
 	
@@ -65,11 +92,22 @@ int main( int argc, char *argv[] ) {
 	apply_surface (SCREEN_WIDTH/2, SCREEN_HEIGHT/2, bg, screen);
 	apply_surface (180, 140, dao, screen);
 	
-	SDL_Flip( screen );
+	if (SDL_Flip( screen ) == -1) {
+		printf ("%s %d ERROR: Unable to flip screen.\n", __PRETTY_FUNCTION__, __LINE__);
+		return 1;
+	}
 	
+	while( quit == false ) {
+		while( SDL_PollEvent( &event ) ) {
+			if( event.type == SDL_QUIT ) {
+				printf ("%s %d Quit event decected.\n", __PRETTY_FUNCTION__, __LINE__);
+				quit = true;
+			}
+		}
+	}
     // Just keep the main window open for 5 seconds then close.
-    SDL_Delay( 5000 );
     
+//    SDL_Delay( 5000 );
 	SDL_FreeSurface(dao);
 	SDL_FreeSurface(bg);
     SDL_Quit();
